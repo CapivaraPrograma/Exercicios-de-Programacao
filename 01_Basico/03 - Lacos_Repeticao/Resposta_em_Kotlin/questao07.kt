d62eafcca237fc1f55cc0404377cb3ef3fd18336
capivara programa <capivaraviralata@gmail.com>
2024-01-07 18:22:07 -0300

    finalizando a questão 07

---
+++ 01_Basico/03 - Lacos_Repeticao/Resposta_em_Kotlin/questao07.kt
@@ -0,0 +1,20 @@
+fun main(){
+    println("Digite um número para usar na sequência de fibonacci")
+    var numero = readLine()!!.toInt()
+    var ultimo = 1
+    var penultimo = 1
+
+    if (numero == 1 || numero == 2){
+        println("1")
+    }else{
+        var count=3
+        while (count <= numero){
+            println("Você está no termo: $count")
+            var termo = ultimo + penultimo
+            penultimo = ultimo
+            ultimo = termo
+            count += 1
+            println(termo)
+        }
+    }
+}
