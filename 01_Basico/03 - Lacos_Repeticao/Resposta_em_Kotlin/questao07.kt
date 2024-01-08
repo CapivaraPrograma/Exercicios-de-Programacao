9a84a2fdb18bd48c4e5fc55666cc180038aa5ee3
capivara programa <capivaraviralata@gmail.com>
2024-01-08 20:52:36 -0300

    questao 07 em kotlin

--- 01_Basico/03 - Lacos_Repeticao/Resposta_em_Kotlin/questao07.kt
+++ 01_Basico/03 - Lacos_Repeticao/Resposta_em_Kotlin/questao07.kt
@@ -1,14 +1,14 @@
-fun main(){
+fun main() {
     println("Digite um número para usar na sequência de fibonacci")
     var numero = readLine()!!.toInt()
     var ultimo = 1
     var penultimo = 1
 
-    if (numero == 1 || numero == 2){
+    if (numero == 1 || numero == 2) {
         println("1")
-    }else{
-        var count=3
-        while (count <= numero){
+    } else {
+        var count = 3
+        while (count <= numero) {
             println("Você está no termo: $count")
             var termo = ultimo + penultimo
             penultimo = ultimo
---
+++ 01_Basico/03 - Lacos_Repeticao/Resposta_em_Kotlin/questao08.kt
@@ -0,0 +1,21 @@
+fun manin(){
+    var maior = -1000
+    var menor = 1000
+    while (True) {
+        valor = input("\nSe deseja parar digite 'parar'\nDigite um número entre 0 e 1000: ")
+        
+        if (valor == "parar"):
+
+            break
+        else:
+            numero = int(valor)
+            if (numero > maior):
+                maior = numero
+            else:
+                if (numero < menor):
+                    menor = numero
+    }
+    
+                
+    print(f"O maior número é {maior} e o menor número é {menor}")
+}
