961e8beabd626e198002bbe0547b7161fb9407aa
Capivara <capivaraviralata@gmail.com>
2024-07-14 11:57:43 -0300

    correção no nome do arquivo

--- 01_Basico/01 - Conceitos_Basicos/Resposta_em_Java/SomarNumeros.java
+++ 01_Basico/01 - Conceitos_Basicos/Resposta_em_Java/SomarNumeros.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; // Importa a classe Scanner da biblioteca java.util para ler a entrada do usuário
 
-public class SomarNUmeros {
+public class SomarNumeros {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Cria um objeto Scanner para ler a entrada do usuário
         
@@ -9,7 +9,7 @@ public class SomarNUmeros {
         System.out.print("Digite outro número: "); // Exibe uma mensagem pedindo um número
         int numero2 = scanner.nextInt(); // Lê a linha digitada pelo usuário e armazena na variável como int
         int soma = numero1 + numero2; // Realiza a soma dos 2 números
-        System.out.println("A soma de: " + numero1 + " e " + numero2 + " é soma: " + soma); // Exibe uma mensagem de boas-vindas usando o nome fornecido
+        System.out.println("A soma de: " + numero1 + " e " + numero2 + " é soma " + soma); // Exibe uma mensagem de soma
         
         scanner.close(); // Fecha o objeto Scanner para liberar os recursos
     }
